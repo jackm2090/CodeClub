5093123ecb5517572b48fcd2a1db34b227900a64
jackm2090 <jackm2090@gmail.com>
2016-10-10 14:54:01 -0400

    Create HelloWorld.cs

---
+++ HelloWorld.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Threading;
+
+namespace HelloWorld {
+  class Program {
+    public static void Main(string[] args) {
+      Console.WriteLine("Hello world!");
+    }
+  }
+}
